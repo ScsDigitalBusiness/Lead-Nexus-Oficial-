718a12bb91c7999756659eb5a6d8d6d02e5697a9
Thaigo Developer <duartedigitalwork@gmail.com>
2024-05-17 08:28:14 -0300

    att

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const globalMiddleware = require ('./src/middlewares/middlewares'); 
 const app = express();
-const port = 10000;
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
