3cfdd6b9569f4c7b96d5d2e6baff9a6446566d14
Thaigo Developer <duartedigitalwork@gmail.com>
2024-05-24 11:25:08 -0300

    att

--- src/models/ProductAndServiceModel.js
+++ src/models/ProductAndServiceModel.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const ProductAndServiceSchema= mongoose.Schema({
   name: {type:String, required: true}, 
   category: {type:String,required: true }, 
-  sku: {type:String, required:true}, 
-  
+  description: {type:String, required: true}, 
+  sku: {type:String, required:true},  
+  weight: {type: String, required: true}, 
+  brand: {type: String, required: false},  
+  date: {type:String, required: true}, 
+
 })
