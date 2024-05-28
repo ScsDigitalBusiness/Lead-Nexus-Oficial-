59b6df4f273db66ce7b32f9a3f5b007eee98acef
Thaigo Developer <duartedigitalwork@gmail.com>
2024-05-28 19:40:47 -0300

    att

--- src/controllers/DashboardControllers.js
+++ src/controllers/DashboardControllers.js
@@ -5,13 +5,13 @@ const Category = require("../models/CategoryMOdel");
 exports.indexDashboard =  async(req,res) =>{   
     const AllLeads = new Leads(req.body,req.session.user); 
     const allLeadsOnDB = await  AllLeads.getAllNumberOfLeadsRegisterForUser();    
-    const leads = await AllLeads.getLeads()
+    const leadsAll = await AllLeads.getLeads(); 
     const emails = await  AllLeads.getAllEmails();   
     const AllLeadsInMonth = await AllLeads.getAllLeadsInThisMonth();  
     const category =  new Category(req.body);  
     const process = new Process(req.body);  
     const categories =  await  category.getAllCategory();     
     const allProcess = await process.getAllProcess(); 
-    res.render('Dashboard',{emails,allLeadsOnDB,leads,AllLeadsInMonth,categories,allProcess});
+    res.render('Dashboard',{emails,allLeadsOnDB,leadsAll,AllLeadsInMonth,categories,allProcess});
 }; 
  
